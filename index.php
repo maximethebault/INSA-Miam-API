03948f8611eadebd20e6724cc7b1a2977bbd4bd3
Maxime <contact@maximethebault.me>
2014-10-07 23:34:15 +0200

    Change model directory

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ header("Expires: Mon, 01 Jan 1970 05:00:00 GM");
 require 'libs/ActiveRecord/ActiveRecord.php';
 
 $cfg = ActiveRecord\Config::instance();
-$cfg->set_model_directory(__DIR__ . '/Model');
+$cfg->set_model_directory(__DIR__ . '/model');
 $cfg->set_connections(array('production' => 'mysql://' . $config['db_username'] . ':' . $config['db_password'] . '@' . $config['db_host'] . '/' . $config['db_database'] . ';charset=utf8'));
 
 require 'controller/Controller.php';
