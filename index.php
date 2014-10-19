68f80a1b6ee7b8c8e2d4446ea5b5ae5edfb9fd26
Maxime <contact@maximethebault.me>
2014-10-20 00:16:16 +0200

    Further refractoring

--- index.php
+++ index.php
@@ -6,14 +6,13 @@ set_time_limit(30);
 
 header("Expires: Mon, 01 Jan 1970 05:00:00 GM");
 
-require 'libs/ActiveRecord/ActiveRecord.php';
+require 'vendor/autoload.php';
 require_once 'config.php';
 
 $cfg = ActiveRecord\Config::instance();
-$cfg->set_model_directory(__DIR__ . '/model');
+$cfg->set_model_directory(__DIR__ . '/src/Model');
 $cfg->set_connections(array('production' => 'mysql://' . $config['db_username'] . ':' . $config['db_password'] . '@' . $config['db_host'] . '/' . $config['db_database'] . ';charset=utf8'));
 $cfg->set_default_connection('production');
 
-require 'controller/Controller.php';
 $controller = new Controller();
 $controller->run();
